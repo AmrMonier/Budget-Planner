7fe55efe002cab0e49bd90e11862c8e12a877c11
Amr Monier <amr.moneir@contact.eg>
2023-06-01 02:31:06 +0200

    added axios

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
+import App from '@/App.vue'
 import router from './router'
 
 // Nucleo Icons
@@ -8,9 +8,10 @@ import './assets/css/nucleo-icons.css'
 import './assets/css/nucleo-svg.css'
 
 import materialKit from './material-kit'
+import axiosInstance from '@/services/axios.service'
 
 const app = createApp(App)
-
+app.config.globalProperties.$axios = axiosInstance
 app.use(createPinia())
 app.use(router)
 app.use(materialKit)
