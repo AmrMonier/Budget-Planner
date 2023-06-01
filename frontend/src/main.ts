7111775431788ce3ac36fdd2f4a2b2223ca5e424
Amr Monier <amr.moneir@contact.eg>
2023-06-01 03:22:16 +0200

    added sakaia theme

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,15 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
 
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/styles.scss';
+
 const app = createApp(App)
+app.use(PrimeVue, { ripple: true });
 
 app.use(createPinia())
 app.use(router)
